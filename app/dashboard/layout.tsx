6287b57ad6500ec01b9dcef75ab7a378e9931637
Kom Senapati <komnoob123@gmail.com>
2024-11-23 15:00:26 +0000

    🔥dashboard init

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { DashboardSidebar } from "@/components/dashboard-sidebar";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { usePathname } from "next/navigation";
+import { ModeToggle } from "@/components/theme/theme-toggle";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const location = usePathname();
+  const breadcrumbSegments = location
+    .split("/")
+    .filter(Boolean)
+    .map((segment) =>
+      segment.replace(/-/g, " ").replace(/\b\w/g, (char) => char.toUpperCase())
+    );
+
+  return (
+    <SidebarProvider>
+      <DashboardSidebar />
+      <SidebarInset>
+        <header className="flex sticky top-0 bg-background h-16 shrink-0 items-center justify-between gap-2 border-b px-4">
+          <div className="flex items-center">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                {breadcrumbSegments.map((segment, index) => (
+                  <>
+                    {index === breadcrumbSegments.length - 1 ? (
+                      <BreadcrumbItem key={index}>
+                        <BreadcrumbPage>{segment}</BreadcrumbPage>
+                      </BreadcrumbItem>
+                    ) : (
+                      <>
+                        <BreadcrumbItem key={index}>
+                          <BreadcrumbLink
+                            href={`/${breadcrumbSegments
+                              .slice(0, index + 1)
+                              .join("/")
+                              .toLowerCase()}`}
+                          >
+                            {segment}
+                          </BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator />
+                      </>
+                    )}
+                  </>
+                ))}
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+          <ModeToggle />
+        </header>
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
---
+++ components/dashboard-sidebar.tsx
@@ -0,0 +1,81 @@
+import * as React from "react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import {
+  GitBranch,
+  GitPullRequest,
+  LayoutDashboard,
+  Settings,
+} from "lucide-react";
+
+const items = [
+  {
+    title: "Repository",
+    url: "/dashboard/repository",
+    icon: GitBranch,
+  },
+  {
+    title: "Workflow",
+    url: "/dashboard/workflow",
+    icon: GitPullRequest,
+  },
+  {
+    title: "Settings",
+    url: "/dashboard/settings",
+    icon: Settings,
+  },
+];
+
+export function DashboardSidebar({
+  ...props
+}: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props} variant="floating">
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <LayoutDashboard className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-semibold">Dashboard</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
