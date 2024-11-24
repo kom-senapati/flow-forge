1a322fc20f3b09615aca2788c58d630b89f502a9
Kom Senapati <komnoob123@gmail.com>
2024-11-24 14:50:17 +0000

    😏 landing page

---
+++ components/landing-page/Footer.tsx
@@ -0,0 +1,41 @@
+export const Footer = () => {
+  return (
+    <footer className="py-5 bg-black text-white/60 border-t border-white/20">
+      <div className="flex flex-col gap-5 sm:flex-row sm:justify-between">
+        <div className="text-center">
+          2024 Eldora UI All rights are reserved
+        </div>
+        <ul className="flex justify-center gap-2.5">
+          <li>
+            <img
+              src="../../assets/icons/x-social.svg"
+              alt="X (formerly Twitter)"
+              className="w-6 h-6"
+            />
+          </li>
+          <li>
+            <img
+              src="../../assets/icons/linkedin.svg"
+              alt="LinkedIn"
+              className="w-6 h-6"
+            />
+          </li>
+          <li>
+            <img
+              src="../../assets/icons/insta.svg"
+              alt="Instagram"
+              className="w-6 h-6"
+            />
+          </li>
+          <li>
+            <img
+              src="../../assets/icons/youtube.svg"
+              alt="YouTube"
+              className="w-6 h-6"
+            />
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
